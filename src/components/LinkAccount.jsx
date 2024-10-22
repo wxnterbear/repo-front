a65e2e8377d28f25c3bbf56df8866fc97c17693a
Wavyboo <wxnterbear04@gmail.com>
2024-10-21 19:08:29 -0500

    21oct

--- src/components/LinkAccount.jsx
+++ src/components/LinkAccount.jsx
@@ -7,34 +7,49 @@ const LinkAccount = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [error, setError] = useState(null);
-    const [hasCheckedGoogle, setHasCheckedGoogle] = useState(false);
     const [token, setToken] = useState(localStorage.getItem('token'));
+
+    // Estado para Google
+    const [hasCheckedGoogle, setHasCheckedGoogle] = useState(false);
     const [isGoogleLinked, setIsGoogleLinked] = useState(localStorage.getItem('isGoogleLinked') === 'true');
+
+    // Estado para Meta
+    const [hasCheckedMeta, setHasCheckedMeta] = useState(false);
+    const [isMetaLinked, setIsMetaLinked] = useState(localStorage.getItem('isMetaLinked') === 'true');
 
     useEffect(() => {
         if (!token) {
-            console.error('No se encontró el token de autenticación');
             setError('No se encontró el token de autenticación. Por favor, inicia sesión nuevamente.');
             return;
         }
 
         const queryString = location.search;
+
+        // Verificar el callback de Google
         if (!hasCheckedGoogle && queryString) {
             const params = new URLSearchParams(queryString);
             const code = params.get('code');
             const state = params.get('state');
-
             if (code && state) {
-                handleOauthCallback(queryString);
+                handleOauthCallbackGoogle(queryString);
                 setHasCheckedGoogle(true);
             }
         }
-    }, [hasCheckedGoogle, location.search, token]);
+
+        // Verificar el callback de Meta
+        if (!hasCheckedMeta && queryString) {
+            const params = new URLSearchParams(queryString);
+            const code = params.get('code');
+            const state = params.get('state');
+            if (code && state) {
+                handleOauthCallbackMeta(queryString);
+                setHasCheckedMeta(true);
+            }
+        }
+    }, [hasCheckedGoogle, hasCheckedMeta, location.search, token]);
 
     const handleLinkGoogle = async () => {
         try {
-            const token = localStorage.getItem('token');
-
             const response = await fetch('https://django-tester.onrender.com/auth/google/', {
                 method: 'GET',
                 headers: {
@@ -47,7 +62,7 @@ const LinkAccount = () => {
             console.log('Respuesta del servidor (link):', data);
 
             if (data) {
-                window.location.href = data;
+                window.location.href = data; // Redirige a la autenticación de Google
             } else {
                 setError('Error al obtener la URL de autenticación de Google');
             }
@@ -56,30 +71,46 @@ const LinkAccount = () => {
         }
     };
 
-    const handleUnlinkGoogle = async () => {
+    const handleLinkMeta = async () => {
         try {
-            const token = localStorage.getItem('token');
-    
-            const response = await fetch('https://django-tester.onrender.com/auth/google/unlink', { // Cambiar a la URL correcta
-                method: 'GET',  // Cambia a 'GET' si es lo que acepta el endpoint
+            const response = await fetch('https://www.facebook.com/v17.0/dialog/oauth?client_id=1360414881319473&redirect_uri=https%3A%2F%2Frepo-front-o1hw.onrender.com%2Fauth%2Fmeta%2F&scope=email%2Cpages_manage_cta%2Cpages_manage_instant_articles%2Cpages_manage_engagement%2Cpages_manage_posts%2Cpages_read_engagement%2Cpublish_video%2Cinstagram_basic%2Cinstagram_shopping_tag_products%2Cinstagram_content_publish&response_type=code&ret=login&fbapp_pres=0&logger_id=41cf9ed8-b228-4b0f-af1e-a2806bd3a321&tp=unspecified&cbt=1725916819679&ext=1725920435&hash=AeZAyJGld3iQbPmNgr4', {
+                method: 'GET',
                 headers: {
                     'Authorization': `Token ${token}`,
                     'Content-Type': 'application/json',
                 },
             });
-    
+
+            const data = await response.json();
+            if (data) {
+                window.location.href = data; // Redirige a la autenticación de Meta
+            } else {
+                setError('Error al obtener la URL de autenticación de Meta');
+            }
+        } catch (error) {
+            setError('Error al iniciar el proceso de vinculación con Meta');
+        }
+    };
+
+    const handleUnlinkGoogle = async () => {
+        try {
+            const response = await fetch('https://django-tester.onrender.com/auth/google/unlink', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+
             if (response.ok) {
-                localStorage.removeItem('isGoogleLinked'); // Eliminar del localStorage
-                setIsGoogleLinked(false); // Actualizar estado
-    
+                localStorage.removeItem('isGoogleLinked');
+                setIsGoogleLinked(false);
                 Swal.fire({
                     title: 'Desvinculación exitosa!',
                     text: 'La cuenta de Google ha sido desvinculada con éxito',
                     icon: 'success',
                     confirmButtonText: 'Aceptar'
-                }).then(() => {
-                    navigate('/link_account');
-                });
+                }).then(() => navigate('/link_account'));
             } else {
                 const errorData = await response.json();
                 setError(`Error al desvincular la cuenta de Google: ${errorData.message}`);
@@ -88,11 +119,36 @@ const LinkAccount = () => {
             setError('Error al procesar la solicitud de desvinculación');
         }
     };
-    
 
-    const handleOauthCallback = async (queryString) => {
-        console.log('Procesando callback con queryString:', queryString);
+    const handleUnlinkMeta = async () => {
+        try {
+            const response = await fetch('https://django-tester.onrender.com/auth/meta/unlink', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
 
+            if (response.ok) {
+                localStorage.removeItem('isMetaLinked');
+                setIsMetaLinked(false);
+                Swal.fire({
+                    title: 'Desvinculación exitosa!',
+                    text: 'La cuenta de Meta ha sido desvinculada con éxito',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                }).then(() => navigate('/link_account'));
+            } else {
+                const errorData = await response.json();
+                setError(`Error al desvincular la cuenta de Meta: ${errorData.message}`);
+            }
+        } catch (error) {
+            setError('Error al procesar la solicitud de desvinculación');
+        }
+    };
+
+    const handleOauthCallbackGoogle = async (queryString) => {
         const url = `https://django-tester.onrender.com/auth/google/oauth2callback${queryString}`;
 
         try {
@@ -106,18 +162,9 @@ const LinkAccount = () => {
 
             if (response.ok) {
                 const result = await response.json();
-                console.log('Resultado del servidor:', result);
                 setIsGoogleLinked(true);
                 localStorage.setItem('isGoogleLinked', 'true');
-
-                Swal.fire({
-                    title: 'Vinculación exitosa!',
-                    text: 'La cuenta de Google ha sido vinculada con éxito',
-                    icon: 'success',
-                    confirmButtonText: 'Aceptar'
-                }).then(() => {
-                    navigate('/link_account');
-                });
+                Swal.fire('Vinculación exitosa!', 'La cuenta de Google ha sido vinculada', 'success');
             } else {
                 const errorData = await response.json();
                 setError(`Error al procesar el callback de Google: ${errorData.message}`);
@@ -127,9 +174,35 @@ const LinkAccount = () => {
         }
     };
 
+    const handleOauthCallbackMeta = async (queryString) => {
+        const url = `https://django-tester.onrender.com/auth/meta/oauth2callback${queryString}`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                setIsMetaLinked(true);
+                localStorage.setItem('isMetaLinked', 'true');
+                Swal.fire('Vinculación exitosa!', 'La cuenta de Meta ha sido vinculada', 'success');
+            } else {
+                const errorData = await response.json();
+                setError(`Error al procesar el callback de Meta: ${errorData.message}`);
+            }
+        } catch (error) {
+            setError('Error en la solicitud al servidor');
+        }
+    };
+
     useEffect(() => {
-        const storedIsGoogleLinked = localStorage.getItem('isGoogleLinked') === 'true';
-        setIsGoogleLinked(storedIsGoogleLinked);
+        setIsGoogleLinked(localStorage.getItem('isGoogleLinked') === 'true');
+        setIsMetaLinked(localStorage.getItem('isMetaLinked') === 'true');
     }, []);
 
     return (
@@ -138,13 +211,14 @@ const LinkAccount = () => {
             {error && <p style={{ color: 'red' }}>{error}</p>}
             <center>
                 {isGoogleLinked ? (
-                    <button onClick={handleUnlinkGoogle}>
-                        Desvincular cuenta de Google
-                    </button>
+                    <button onClick={handleUnlinkGoogle}>Desvincular cuenta de Google</button>
                 ) : (
-                    <button onClick={handleLinkGoogle}>
-                        Iniciar Vinculación con Google
-                    </button>
+                    <button onClick={handleLinkGoogle}>Iniciar Vinculación con Google</button>
+                )}
+                {isMetaLinked ? (
+                    <button onClick={handleUnlinkMeta}>Desvincular cuenta de Meta</button>
+                ) : (
+                    <button onClick={handleLinkMeta}>Iniciar Vinculación con Meta</button>
                 )}
             </center>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,28 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import '../css/header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar el menú
     const navigate = useNavigate();
-    const { logout } = useContext(AuthContext); // Usamos logout en lugar de setIsAuthenticated
+    const { logout } = useContext(AuthContext);
 
     const handleLogout = () => {
-        logout(); // Llamamos a la función logout del contexto
-        navigate('/login'); // Redirige al login
+        logout();
+        navigate('/login');
     };
 
     const isLoggedIn = !!localStorage.getItem('token');
 
+    const toggleMenu = () => {
+        setMenuOpen(prevState => !prevState);
+    };
+
     return (
-        <div className="header">
-            <button className="opc" onClick={() => navigate('/brainstorming')}>Ir a lluvia de ideas</button>
-            <button className="opc" onClick={() => navigate('/proposals')}>Ir a Propuestas</button>
-            <button className="opc" onClick={() => navigate('/proposals_form')}>Ir al formulario de Propuestas</button>
-            <button className="opc" onClick={() => navigate('/calendar')}>Ir a Calendario</button>
-            {isLoggedIn && (
-                <button className="opc" onClick={handleLogout}>Cerrar sesión</button>
-            )}
+        <div className={`header ${menuOpen ? 'header-expanded' : ''}`}>
+            <button className="hamburger" onClick={toggleMenu}>☰</button>
+            <div className={`menu ${menuOpen ? 'open' : ''}`}>
+                <button className="opc" onClick={() => navigate('/brainstorming')}>Ir a lluvia de ideas</button>
+                <button className="opc" onClick={() => navigate('/proposals')}>Ir a Propuestas</button>
+                <button className="opc" onClick={() => navigate('/proposals_form')}>Ir al formulario de Propuestas</button>
+                <button className="opc" onClick={() => navigate('/calendar')}>Ir a Calendario</button>
+                {isLoggedIn && (
+                    <button className="opc" onClick={handleLogout}>Cerrar sesión</button>
+                )}
+            </div>
         </div>
     );
 };
